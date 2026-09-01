7babd007d6d0a5d236e15a3ded02480839b20bd1
todopropi <163132046+todopropi@users.noreply.github.com>
2026-09-01 13:21:05 +0200

    Add initial question to bancoActualitat
    Initialized bancoActualitat with a sample question and options.

--- Actualidad_preguntas.js
+++ Actualidad_preguntas.js
@@ -1,3 +1,16 @@
 window.bancoActualitat = [
-
+ {
+    "id": "ACT_CUSTOM_1788261516387_724",
+    "pregunta": "Qui ha guanyat les 500 Milles d'Indianapolis l'any 2026?",
+    "opcions": [
+      "Álex Palou",
+      "Fernando Alonso",
+      "Carlos Sainz",
+      "Pedro de la Rosa"
+    ],
+    "resposta": 0,
+    "explicacio": "Ha guanyat 5 campionats.",
+    "categoria": "Esports",
+    "seccio": "Piloto"
+  }
 ];
